eaf59e1bda5af9f5fb208324c4be91dda80f92ad
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-12-19 14:00:55 +0530

    Add DropDown Widget

---
+++ lib/widgets/dropdown.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DropDown extends StatelessWidget {
+  final Function onSelect;
+  final List<Map<String, String>> cities;
+
+  DropDown(this.onSelect, this.cities);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: DropdownButton(
+        hint: Text('Your City'),
+        items: cities
+            .map(
+              (item) => new DropdownMenuItem(
+                value: item['id'],
+                child: Text("${item['name']}", softWrap: true),
+              ),
+            )
+            .toList(),
+        onChanged: (val) {
+          onSelect(val);
+        },
+      ),
+    );
+  }
+}
