5f09611d42162c51ecad52dfe27499a1cec4bfbd
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-12-19 14:00:23 +0530

    Add Place screen to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ghummakad/screens/place_screen.dart';
 
 import './screens/home_screen.dart';
 import './screens/login_screen.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
       routes: {
         SignupScreen.routeName: (ctx) => SignupScreen(),
         LoginScreen.routeName: (ctx) => LoginScreen(),
+        PlaceScreen.routeName: (ctx) => PlaceScreen(),
       },
     );
   }
