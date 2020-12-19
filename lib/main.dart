5f93cf98a3101bfab1b01026d1696bc73ebb2ff4
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-12-19 10:01:32 +0530

    Adds firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ghummakad/screens/login_screen.dart';
 import './screens/home_screen.dart';
 import './screens/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
